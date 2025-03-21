c81eed22f9afb18803d65f898cf5a03a3d2b9c3a
Abulfadl Ahmadi <abulfadl.ahmadi@gmail.com>
2025-03-21 20:46:01 +0330

    fix url for media files

--- backend/OnlineShop/urls.py
+++ backend/OnlineShop/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/catalog/', include('catalog.urls')),
     path('api/', include('core.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
